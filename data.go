d57491fef35993608705909b9afc9df06b1d4dd0
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2022-07-16 10:27:19 +0200

    add data type (#2)

---
+++ data.go
@@ -0,0 +1,47 @@
+package si
+
+const (
+	Bit      Data = 1e0
+	Yoctobit      = Yocto * Bit
+	Zeptobit      = Zepto * Bit
+	Attobit       = Atto * Bit
+	Femtobit      = Femto * Bit
+	Picobit       = Pico * Bit
+	Nanobit       = Nano * Bit
+	Microbit      = Micro * Bit
+	Millibit      = Milli * Bit
+	Centibit      = Centi * Bit
+	Decibit       = Deci * Bit
+	Decabit       = Deca * Bit
+	Hectobit      = Hecto * Bit
+	Kilobit       = Kilo * Bit
+	Megabit       = Mega * Bit
+	Gigabit       = Giga * Bit
+	Terabit       = Tera * Bit
+	Petabit       = Peta * Bit
+	Exabit        = Exa * Bit
+	Zettabit      = Zetta * Bit
+	Yottabit      = Yotta * Bit
+
+	Byte      Data = Bit * 8
+	Yoctobyte      = Yocto * Byte
+	Zeptobyte      = Zepto * Byte
+	Attobyte       = Atto * Byte
+	Femtobyte      = Femto * Byte
+	Picobyte       = Pico * Byte
+	Nanobyte       = Nano * Byte
+	Microbyte      = Micro * Byte
+	Millibyte      = Milli * Byte
+	Centibyte      = Centi * Byte
+	Decibyte       = Deci * Byte
+	Decabyte       = Deca * Byte
+	Hectobyte      = Hecto * Byte
+	Kilobyte       = Kilo * Byte
+	Megabyte       = Mega * Byte
+	Gigabyte       = Giga * Byte
+	Terabyte       = Tera * Byte
+	Petabyte       = Peta * Byte
+	Exabyte        = Exa * Byte
+	Zettabyte      = Zetta * Byte
+	Yottabyte      = Yotta * Byte
+)
