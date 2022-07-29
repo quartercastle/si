271359fc84a4c5cb2e5d13d8c0ae6f492b0c12ad
quartercastle <hello@kvartborg.net>
2022-07-29 15:09:03 +0000

    Add volume

---
+++ volume.go
@@ -0,0 +1,47 @@
+package si
+
+const (
+	CubicMeter      Volume = 1e0
+	YoctoCubicmeter        = Yocto * CubicMeter
+	ZeptoCubicmeter        = Zepto * CubicMeter
+	AttoCubicmeter         = Atto * CubicMeter
+	FemtoCubicmeter        = Femto * CubicMeter
+	PicoCubicmeter         = Pico * CubicMeter
+	NanoCubicmeter         = Nano * CubicMeter
+	MicroCubicmeter        = Micro * CubicMeter
+	MilliCubicmeter        = Milli * CubicMeter
+	CentiCubicmeter        = Centi * CubicMeter
+	DeciCubicmeter         = Deci * CubicMeter
+	DecaCubicmeter         = Deca * CubicMeter
+	HectoCubicmeter        = Hecto * CubicMeter
+	KiloCubicmeter         = Kilo * CubicMeter
+	MegaCubicmeter         = Mega * CubicMeter
+	GigaCubicmeter         = Giga * CubicMeter
+	TeraCubicmeter         = Tera * CubicMeter
+	PetaCubicmeter         = Peta * CubicMeter
+	ExaCubicmeter          = Exa * CubicMeter
+	ZettaCubicmeter        = Zetta * CubicMeter
+	YottaCubicmeter        = Yotta * CubicMeter
+
+	Liter      Volume = CubicMeter * 1e-3
+	Yoctoliter        = Yocto * Liter
+	Zeptoliter        = Zepto * Liter
+	Attoliter         = Atto * Liter
+	Femtoliter        = Femto * Liter
+	Picoliter         = Pico * Liter
+	Nanoliter         = Nano * Liter
+	Microliter        = Micro * Liter
+	Milliliter        = Milli * Liter
+	Centiliter        = Centi * Liter
+	Deciliter         = Deci * Liter
+	Decaliter         = Deca * Liter
+	Hectoliter        = Hecto * Liter
+	Kiloliter         = Kilo * Liter
+	Megaliter         = Mega * Liter
+	Gigaliter         = Giga * Liter
+	Teraliter         = Tera * Liter
+	Petaliter         = Peta * Liter
+	Exaliter          = Exa * Liter
+	Zettaliter        = Zetta * Liter
+	Yottaliter        = Yotta * Liter
+)
